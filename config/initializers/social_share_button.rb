3420fddf9bc07de51dd96ee46df02a8ae6114782
Jacob Murphy <murphorum@gmail.com>
2016-02-07 11:02:58 -0500

    Change social sites allowed for sharing

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -1,3 +1,3 @@
 SocialShareButton.configure do |config|
-  config.allow_sites = %w(facebook twitter tumblr google_plus delicious pinterest email)
+  config.allow_sites = %w(facebook twitter tumblr)
 end
